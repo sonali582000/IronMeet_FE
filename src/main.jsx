983cb6939f2501dabf4b93a94865f1c1dce88c5e
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2024-01-26 16:59:44 +0100

    fetching all events done

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AuthContextProvider from './contexts/AuthContext.jsx'
+import EventContextProvider from './contexts/EventContext.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
+        <EventContextProvider>
+          <App />
+        </EventContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
