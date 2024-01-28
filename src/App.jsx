0480584b746fecdc0a253528ec6d42f6a2774b4d
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2024-01-28 15:10:44 +0100

    fetch doenst work ?

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
+import EventDetails from './pages/EventDetailsPage';
 import './App.css';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path='/signup' element={<SignUpPage />} />
           <Route path='/login' element={<LoginPage />} /> 
+          <Route path='/eventDetail/:eventId' element={<EventDetails />} /> 
         </Routes>
         <Footer />
       </div>
--- src/contexts/EventContext.jsx
+++ src/contexts/EventContext.jsx
@@ -5,17 +5,30 @@ export const EventContext = createContext();
 
 const EventContextProvider = ({children}) => {
     const [events, setEvents] = useState([]);
+    const [event, setEvent] = useState([]);
 
     const fetchEvents = async () => {
         try {
 
             const response = await axios.get(`${import.meta.env.VITE_API_URL}/event`)
 
-            console.log(response.data)
-
             if(response === 200){
-                const eventsData = response.eventsData
+                const eventsData = response.data
                 setEvents(eventsData)
+            }
+            
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const fetchOneEvent = async (_id) =>{
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_API_URL}/event/${_id}`)
+            if(response === 200){
+                const eventData = response.data
+                setEvent(eventData)
+                console.log("Fetched event:", eventData);
             }
             
         } catch (error) {
@@ -25,11 +38,12 @@ const EventContextProvider = ({children}) => {
 
     useEffect(()=>{
         fetchEvents()
+        console.log("Events:",events)
     },[])
 
 
     return(
-        <EventContext.Provider value={{fetchEvents, events}}>
+        <EventContext.Provider value={{fetchEvents, events, fetchOneEvent, event}}>
             {children}
         </EventContext.Provider>
     )
