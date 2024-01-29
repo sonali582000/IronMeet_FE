9002029b223f9d61137bb0d84cd7198230d87303
Julietadallapozza <dallapozzajulieta@gmail.com>
2024-01-29 11:35:22 +0100

    wip

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { EventContext } from '../contexts/EventContext';
 
 function HomePage() {
