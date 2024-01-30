c9e58044989745b705ccb1d697095f2d0bafe79b
Julietadallapozza <dallapozzajulieta@gmail.com>
2024-01-30 16:44:41 +0100

    navbar styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,14 @@ import styles from '../styles/Navbar.module.css'
 
 function Navbar() {
     return (
-        <nav className={styles.navbar}> 
-            <img src={logo} className={styles.logo} alt="Logo" /> 
-            <div className={styles.links}> 
+        <nav className={styles.navbar}>
+            <img src={logo} className={styles.logo} alt="Logo" />
+            <div className={styles.links}>
                 <Link to="/" className={styles.link}><button>Home</button></Link>
                 <Link to="/about" className={styles.link}><button>About us</button></Link>
                 <Link to="/profile" className={styles.link}><button>Profile</button></Link>
+                <Link to="/signup" className={styles.link}><button>Go to Signup</button></Link>
+                <Link to="/login" className={styles.link}><button>Go to Login</button></Link>
             </div>
         </nav>
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,20 +14,14 @@ function HomePage() {
       <div className={styles.welcomeTitle}>
         <span>Welcome to ironMeet!</span>
       </div>
-      <div className={styles.homeButtonsContainer} >
-        <Link to="/signup">
-          <button className={styles.homeButton}>Go to Signup</button>
-        </Link>
-        <Link to="/login">
-          <button className={styles.homeButton}>Go to Login</button>
-        </Link>
+  
         <ul>
           {events && events.map((event) => (
             <li key={event._id}>{event.title}</li>
           ))}
         </ul>
       </div>
-    </div>
+
   
 
  
