0eff7e27a12c19b18c76cb0d7c7d7389cb4b4ffc
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2024-01-31 18:42:30 +0100

    deployment test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ function Navbar() {
                 <Link to="/profile" className={styles.link}><button>Profile</button></Link>
                 <Link to="/signup" className={styles.link}><button>Signup</button></Link>
                 <Link to="/login" className={styles.link}><button>Login</button></Link>
+                <Link to="/event/new" className={styles.link}><button>Create an event</button></Link>
             </div>
         </nav>
     )
